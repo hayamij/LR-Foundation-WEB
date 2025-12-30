ae571de2c56e634a865167ab16f99d0ed62f0b48
hayamij <phuongtuan.dev@gmail.com>
2025-12-30 22:17:13 +0700

    fixed controllers code style

--- src/controllers/programs.controller.js
+++ src/controllers/programs.controller.js
@@ -1,41 +1,57 @@
 const config = require('../config/app.config');
+const contentService = require('../services/content.service');
 
 exports.getProgramsPage = (req, res) => {
+  const statistics = contentService.getStatistics();
+  
   res.render('pages/programs', {
     title: 'Chương trình - Dự án',
     config: config,
-    page: 'programs'
+    page: 'programs',
+    statistics
   });
 };
 
 exports.getEducationPage = (req, res) => {
+  const educationPrograms = contentService.getEducationPrograms();
+  
   res.render('pages/programs/education', {
     title: 'Hỗ trợ Giáo dục',
     config: config,
-    page: 'programs'
+    page: 'programs',
+    programs: educationPrograms
   });
 };
 
 exports.getHealthcarePage = (req, res) => {
+  const healthcarePrograms = contentService.getHealthcarePrograms();
+  
   res.render('pages/programs/healthcare', {
     title: 'Y tế & Sức khỏe',
     config: config,
-    page: 'programs'
+    page: 'programs',
+    programs: healthcarePrograms
   });
 };
 
 exports.getSocialPage = (req, res) => {
+  const socialPrograms = contentService.getSocialPrograms();
+  
   res.render('pages/programs/social', {
     title: 'Bác ái xã hội',
     config: config,
-    page: 'programs'
+    page: 'programs',
+    programs: socialPrograms
   });
 };
 
 exports.getFundraisingPage = (req, res) => {
+  const fundraisingInfo = contentService.getFundraisingInfo();
+  
   res.render('pages/programs/fundraising', {
     title: 'Gây quỹ',
     config: config,
-    page: 'programs'
+    page: 'programs',
+    fundraising: fundraisingInfo
   });
 };
