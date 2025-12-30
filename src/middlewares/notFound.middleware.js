7e3af911cf52b7d16be9ef0f81cf3c46d57d9e51
hayamij <phuongtuan.dev@gmail.com>
2025-12-30 22:21:43 +0700

    fix middlewares

--- src/middlewares/notFound.middleware.js
+++ src/middlewares/notFound.middleware.js
@@ -1,10 +1,10 @@
 const config = require('../config/app.config');
 
 module.exports = (req, res, next) => {
-  res.status(404).render('pages/404', {
-    title: 'Không tìm thấy trang',
-    url: req.originalUrl,
-    config: config,
-    page: '404'
-  });
+    res.status(404).render('pages/404', {
+        title: 'Không tìm thấy trang',
+        url: req.originalUrl,
+        config: config,
+        page: '404'
+    });
 };
